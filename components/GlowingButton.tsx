b89ad2f793356bba646e706223b47807affb7b6e
Ayush Dixit <fsayush100@gmail.com>
2025-03-26 23:54:11 +0530

    changes in landing page

--- components/GlowingButton.tsx
+++ components/GlowingButton.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link'
 
 const GlowingButton = ({ text = "Get Started" }: { text?: string | React.ReactNode }) => {
     return (
-        <Link href={"/chat"}>
+        <a href={"/chat"} target='_blank'>
             <motion.div whileHover={{ scale: 1.02 }} className="group cursor-pointer relative">
                 <div className="absolute -inset-1 cursor-pointer rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-75 blur transition duration-500 group-hover:opacity-100"></div>
                 <button className="relative flex justify-center items-center gap-1 cursor-pointer text-sm sm:text-base px-5 text-white/80 hover:text-white rounded-lg bg-black sm:px-6 py-2">
                     {text}
                 </button>
             </motion.div>
-        </Link>
+        </a>
 
     )
 }
